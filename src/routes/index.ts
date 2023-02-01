072c3ae19a30abb0498ca74899c42ccbe4d98ef6
Vinicius santos <vinicius.santos@arizona.global>
2023-02-01 13:10:14 -0300

    base criada

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import * as pageController from '../controllers/pageController'
+import * as searchController from '../controllers/searchController'
+import { Request, Response, Router } from "express";
+
+const router = Router()
+
+router.get('/', pageController.home)
+router.get('/dogs', pageController.dogs)
+router.get('/cats', pageController.cats)
+router.get('/fishes', pageController.fishes)
+router.get('/search', searchController.search)
+
+export default router
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mainRoutes from './routes/index'
+import mustache from 'mustache-express'
+import express, {Request, Response} from 'express'
+import path from 'path'
+
+const server = express()
+
+server.set('view engine', 'mustache')
+server.set('views', path.join(__dirname, 'views'))
+server.engine('mustache', mustache())
+
+server.use(express.static(path.join(__dirname, '../public')))
+
+server.use(mainRoutes)
+server.use((req : Request, res : Response) => {
+    res.status(404).send('Página não encontrada!')
+})
+
+server.listen(3000)
